f66cadb0a5da6d4d03f281ae91af790036b9def0
Zack Dithilath <dithilath.aphixath@greenriver.edu>
2023-03-16 06:21:27 -0400

    centered text on nav collasp and created customer class

---
+++ classes/Customer.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * In use with our boba website, this class is design to GET and SET
+ * the user's personal information in order for it to be called and
+ * store into our database
+ */
+class Customer
+{
+
+}
